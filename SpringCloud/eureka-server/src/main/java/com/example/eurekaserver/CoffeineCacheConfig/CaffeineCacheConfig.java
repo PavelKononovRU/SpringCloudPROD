d47996461076ab14175293a2fffc4ca8081a0233
Pavel <lovecars527@gmail.com>
2023-01-12 17:54:32 +0300

    Добавление Eureka Client

---
+++ SpringCloud/eureka-server/src/main/java/com/example/eurekaserver/CoffeineCacheConfig/CaffeineCacheConfig.java
@@ -0,0 +1,27 @@
+package com.example.eurekaserver.CoffeineCacheConfig;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
+@Configuration
+public class CaffeineCacheConfig {
+    @Bean
+    protected Caffeine caffeineConfig() {
+        return Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES);
+    }
+
+    @Bean
+    protected CacheManager cacheManager(Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+
+}
